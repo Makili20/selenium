17c73e7bf76df54dcb6e405fc2fac3a570dad0c8
Marijana Igracki <marijana.makili@gmail.com>
2023-01-12 12:03:45 -0500

    Selenium

---
+++ src/test/java/cybertek/day3_css_Selector_xpath/ExampleForWebDriverFactory.java
@@ -0,0 +1,11 @@
+package cybertek.day3_css_Selector_xpath;
+
+import org.openqa.selenium.WebDriver;
+import utilities.WebDriverFactory;
+
+public class ExampleForWebDriverFactory {
+    public static void main(String[] args) {
+      WebDriver driver= WebDriverFactory.getDriver("chrome");
+driver.get("https://www.google.com");
+    }
+}
