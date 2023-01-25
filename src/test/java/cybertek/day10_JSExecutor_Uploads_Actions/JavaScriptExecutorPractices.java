a4c0a766d0e7e9dabccbd2320567b0afae68a60f
Marijana Igracki <marijana.makili@gmail.com>
2023-01-25 12:18:51 -0500

    Selenium

---
+++ src/test/java/cybertek/day10_JSExecutor_Uploads_Actions/JavaScriptExecutorPractices.java
@@ -0,0 +1,26 @@
+package cybertek.day10_JSExecutor_Uploads_Actions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class JavaScriptExecutorPractices {
+
+    @Test
+    public void jsexecutor_test()throws InterruptedException{
+        Driver.getDriver().get("http://practice.cybertekschool.com/");
+        WebElement cybertekSchoolLink=Driver.getDriver().findElement(By.linkText("CYDEO"));
+        JavascriptExecutor javascriptExecutor=(JavascriptExecutor)Driver.getDriver();
+
+        Thread.sleep(4000);
+        javascriptExecutor.executeScript("arguments[0].scrollIntoView(true);",cybertekSchoolLink);
+    }
+
+
+
+
+
+
+}
---
+++ src/test/java/cybertek/day10_JSExecutor_Uploads_Actions/Uploading.java
@@ -0,0 +1,25 @@
+package cybertek.day10_JSExecutor_Uploads_Actions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class Uploading {
+
+    @Test
+    public void UploadingTest() throws InterruptedException{
+        Driver.getDriver().get("http://practice.cybertekschool.com/upload");
+        WebElement chooseFile=Driver.getDriver().findElement(By.id("file-upload"));
+        Thread.sleep(4000);
+        chooseFile.sendKeys("\"C:\\Users\\marij\\OneDrive\\Desktop\\Test.txt\"");
+        WebElement fileUpload=Driver.getDriver().findElement(By.id("file-submit"));
+        WebElement uploadedMessage=Driver.getDriver().findElement(By.xpath("//div[@class='example']/h3"));
+        Assert.assertTrue(uploadedMessage.isDisplayed());
+
+
+
+
+    }
+}
